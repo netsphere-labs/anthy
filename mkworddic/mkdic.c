1e54ccaebef42c501b9091805d3d4b4c0c7b61e6
unknown <hisashi.horikawa@gmail.com>
2019-08-04 20:12:13 +0900

    fix include headers.

--- mkworddic/mkdic.c
+++ mkworddic/mkdic.c
@@ -32,15 +32,33 @@
  *
  */
 
+#define _CRT_SECURE_NO_WARNINGS
+
+#ifndef _MSC_VER
+  #include <config.h>
+#else
+  #include <defines.h>
+#endif
+
 #include <sys/types.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <ctype.h>
-
-#include <config.h>
+#include <assert.h>
+#ifndef _WIN32
+  #include <unistd.h>
+#else
+  #define STRICT 1
+  #define WIN32_LEAN_AND_MEAN
+  #include <malloc.h> // alloca()
+  #include <windows.h>
+  #include <io.h> // _mktemp()
+  #define strdup _strdup
+  #define fdopen _fdopen
+  #define unlink _unlink
+#endif
 
 #include <anthy/anthy.h>
 #include <anthy/xstr.h>
@@ -152,10 +170,10 @@ flush_output_files (void)
 
 /* ネットワークbyteorderで4bytes書き出す */
 void
-write_nl(FILE *fp, int i)
+write_nl(FILE* fp, uint32_t i)
 {
   i = anthy_dic_htonl(i);
-  fwrite(&i, sizeof(int), 1, fp);
+  fwrite(&i, sizeof(uint32_t), 1, fp);
 }
 
 static void
@@ -677,7 +695,7 @@ find_word_entry(struct yomi_entry_list *yl, xstr *yomi,
   }
   return NULL;
 }
-		
+
 /* 頻度調整のコマンドを適用する */
 static void
 apply_adjust_command(struct yomi_entry_list *yl,
